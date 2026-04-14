03a2e7ca132c813763ef8432b03c92ea276be032
Gokul R <gokul@sarva.ai>
2026-04-14 18:06:36 +0530

    refactor: reverted back deploy devnet script changes

--- participant-intelligence-service/scripts/deploy-devnet.ts
+++ participant-intelligence-service/scripts/deploy-devnet.ts
@@ -50,7 +50,7 @@ async function loadManifest(): Promise<object> {
 }
 
 async function getWallet(): Promise<Wallet> {
-  const mnemonic = "diary spirit praise tail vehicle dune letter day elephant check paddle fat";
+  const mnemonic = process.env.MOI_MNEMONIC;
 
   if (!mnemonic) {
     throw new Error(
